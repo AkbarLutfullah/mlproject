3f91d43b1a10da1bda0a70a72d5264773dd991b6
AkbarLutfullah <akbar.lutfullah@gmail.com>
2023-04-04 16:54:47 +0100

    updated logger and exception

--- src/logger.py
+++ src/logger.py
@@ -4,12 +4,14 @@ from datetime import datetime
 
 # create log file
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(LOG_FILE_PATH, exist_ok=True)
+LOG_DIR = os.path.join(os.path.dirname(__file__), "logs")
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
+os.makedirs(LOG_DIR, exist_ok=True)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
